38b564686bb2ad4b8aedc826ab93c3662f703f1e
hamzazaidi5 <alihamzazaidi75@gmail.com>
2025-02-27 21:16:16 +0500

    name changes

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -308,11 +308,11 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  Where ideas begin
+                <h1 className="text-4xl lg:text-3xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
+                  Build your app in minutes, not months
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  Bring ideas to life in seconds or get help on existing projects.
+                  Bring ideas to life - AI-generated apps and sites ready in minutes, no coding required.
                 </p>
               </div>
             )}
--- app/components/chat/ExamplePrompts.tsx
+++ app/components/chat/ExamplePrompts.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const EXAMPLE_PROMPTS = [
-  { text: 'Build a todo app in React using Tailwind' },
-  { text: 'Build a simple blog using Astro' },
-  { text: 'Create a cookie consent form using Material UI' },
-  { text: 'Make a space invaders game' },
-  { text: 'Make a Tic Tac Toe game in html, css and js only' },
+  { text: 'Build a weather app with a UV index' },
+  { text: 'Build a Personal blog website' },
+  { text: 'Create a dating app for dogs' },
+  { text: 'Make a flappy bird game but with my face' },
+  { text: 'Make a game like Mario' },
 ];
 
 export function ExamplePrompts(sendMessage?: { (event: React.UIEvent, messageInput?: string): void | undefined }) {
